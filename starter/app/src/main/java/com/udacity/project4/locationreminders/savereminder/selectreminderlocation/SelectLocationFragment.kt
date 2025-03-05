8556a5e743adee8e23ae49081d53118211452424
Gil Cunningham <gilcunningham@gmail.com>
2025-03-05 12:55:45 -0500

    Feature/location permissions (#5)
    * - implemented location permissions
    
    * - implemented location permissions

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,18 +1,27 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.PermissionUtils
+import com.udacity.project4.utils.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
+import com.udacity.project4.utils.PermissionUtils.isPermissionGranted
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
@@ -21,9 +30,20 @@ class SelectLocationFragment : BaseFragment() {
 
     // Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
-    private lateinit var binding: FragmentSelectLocationBinding
 
-    private val selectLocationViewModel : SelectLocationViewModel by viewModels()
+    private val selectLocationViewModel: SelectLocationViewModel by viewModels()
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * [.onRequestPermissionsResult].
+     */
+    private var permissionDenied = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("*** SetLocationFragment.onCreate()")
+        println("*** viewModel: $selectLocationViewModel")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,6 +60,7 @@ class SelectLocationFragment : BaseFragment() {
         // TODO: add the map setup implementation
         setupMap()
 
+
         // TODO: zoom to the user location after taking his permission
         // TODO: add style to the map
         // TODO: put a marker to location that the user selected
@@ -48,12 +69,145 @@ class SelectLocationFragment : BaseFragment() {
         //onLocationSelected()
     }
 
+    private fun zoomToCurrentLocation() {
+        println("*** zoom to current location")
+        //val lat = googleMap.myLocation.latitude
+        //val lng = googleMap.myLocation.longitude
+        //println("*** lat = $lat lng = $lng")
+        //val coordinate = LatLng(lat, lng)
+        //val yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 5f)
+        //googleMap.animateCamera(yourLocation)
+        //CameraUpdate
+        //val latLng = LatLng(googleMap.myLocation.latitude, googleMap.myLocation.longitude)
+        //googleMap.moveCamera() fromLatLngZoom()
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation(googleMap: GoogleMap) {
+        println("*** enable localttion")
+        // [START maps_check_location_permission]
+        // 1. Check if permissions are granted, if so, enable the my location layer
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            println("*** location enabled")
+            googleMap.isMyLocationEnabled = true
+            zoomToCurrentLocation()
+            return
+        }
+
+        // 2. If a permission rationale dialog should be shown
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            println("*** new instance")
+            PermissionUtils.RationaleDialog.newInstance(
+                LOCATION_PERMISSION_REQUEST_CODE, true
+            ).show(childFragmentManager, "dialog")
+            return
+        }
+
+        println("*** request permissioss")
+        // 3. Otherwise, request permission
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE
+        )
+        // [END maps_check_location_permission]
+    }
+
+    // [START maps_check_location_permission_result]
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        println("*** onRequestPermissionResult")
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            super.onRequestPermissionsResult(
+                requestCode,
+                permissions,
+                grantResults
+            )
+            return
+        }
+
+        if (isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) || isPermissionGranted(
+                permissions,
+                grantResults,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        ) {
+            // Enable the my location layer if the permission has been granted.
+            //TODO : enableMyLocation()
+            println("*** enableLocation()")
+        } else {
+            // Permission was denied. Display an error message
+            // [START_EXCLUDE]
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true
+            // [END_EXCLUDE]
+        }
+    }
+
+    // [END maps_check_location_permission_result]
+    override fun onResume() {
+        super.onResume()
+        println("*** onResume()")
+        if (permissionDenied) {
+            println("*** permission denied")
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            permissionDenied = false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("**onDestroy")
+        selectLocationViewModel.onMapReady.removeObservers(viewLifecycleOwner)
+    }
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        println("*** showMissingPermissionError()")
+        newInstance(true).show(childFragmentManager, "dialog")
+    }
+
+
     // Get a handle to the fragment and register the callback.
     private fun setupMap() {
+        selectLocationViewModel.onMapReady.observe(viewLifecycleOwner) { googleMap ->
+            println("*** onMapReady")
+            enableMyLocation(googleMap)
+        }
         mapFragment.getMapAsync(selectLocationViewModel)
     }
 
-    private val mapFragment : SupportMapFragment
+    private val mapFragment: SupportMapFragment
         get() = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
     private fun onLocationSelected() {
@@ -71,17 +225,28 @@ class SelectLocationFragment : BaseFragment() {
         R.id.normal_map -> {
             true
         }
+
         R.id.hybrid_map -> {
             true
         }
+
         R.id.satellite_map -> {
             true
         }
+
         R.id.terrain_map -> {
             true
         }
+
         else -> super.onOptionsItemSelected(item)
     }
 
-
+    companion object {
+        /**
+         * Request code for location permission request.
+         *
+         * @see .onRequestPermissionsResult
+         */
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 }
